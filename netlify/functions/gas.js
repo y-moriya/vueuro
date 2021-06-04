48e0a7fa6dc49863cb0b7cf699e545eabf55c4e3
Yu MORIYA <euro21st@gmail.com>
2021-06-04 22:21:20 +0900

    Remove await

--- netlify/functions/gas.js
+++ netlify/functions/gas.js
@@ -1,7 +1,5 @@
 exports.handler = async (event, context) => {
-  setTimeout(() => {
-    await fetch('https://script.google.com/macros/s/AKfycbw5DW0zkMVIzPEIn4pAYnAgklIhVAXR77fBslEs3b--B7xbCaxtEoP0obqTD7VMBCtW/exec');
-  }, 1000);
+  fetch('https://script.google.com/macros/s/AKfycbw5DW0zkMVIzPEIn4pAYnAgklIhVAXR77fBslEs3b--B7xbCaxtEoP0obqTD7VMBCtW/exec');
   return {
     statusCode: 200,
     body: 'Request accepted.',
