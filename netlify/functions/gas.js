c318b1612363cc0cba1f1d7720e720e6c62f6174
Yu MORIYA <euro21st@gmail.com>
2021-06-04 22:18:26 +0900

    Add await

--- netlify/functions/gas.js
+++ netlify/functions/gas.js
@@ -1,6 +1,6 @@
 exports.handler = async (event, context) => {
   setTimeout(() => {
-    fetch('https://script.google.com/macros/s/AKfycbw5DW0zkMVIzPEIn4pAYnAgklIhVAXR77fBslEs3b--B7xbCaxtEoP0obqTD7VMBCtW/exec');
+    await fetch('https://script.google.com/macros/s/AKfycbw5DW0zkMVIzPEIn4pAYnAgklIhVAXR77fBslEs3b--B7xbCaxtEoP0obqTD7VMBCtW/exec');
   }, 1000);
   return {
     statusCode: 200,
