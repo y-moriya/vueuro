e164ace01c62bb0411d0c625286ed5c6631e37ef
Yu MORIYA <euro21st@gmail.com>
2021-06-04 22:44:31 +0900

    Add await to sleep

--- netlify/functions/gas.js
+++ netlify/functions/gas.js
@@ -4,7 +4,7 @@ const _sleep = (ms) => new Promise((resolve) => setTimeout(resolve, ms));
 
 exports.handler = async (event, context) => {
   fetch('https://script.google.com/macros/s/AKfycbw5DW0zkMVIzPEIn4pAYnAgklIhVAXR77fBslEs3b--B7xbCaxtEoP0obqTD7VMBCtW/exec');
-  _sleep(2000);
+  await _sleep(2000);
   return {
     statusCode: 200,
     body: 'Request accepted.',
