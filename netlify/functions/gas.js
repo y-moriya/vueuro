34f5746b6a1f110b8622a2d73a8fa13a4950e172
Yu MORIYA <euro21st@gmail.com>
2021-06-04 22:31:24 +0900

    Add await

--- netlify/functions/gas.js
+++ netlify/functions/gas.js
@@ -1,7 +1,7 @@
 const fetch = require('node-fetch');
 
 exports.handler = async (event, context) => {
-  fetch('https://script.google.com/macros/s/AKfycbw5DW0zkMVIzPEIn4pAYnAgklIhVAXR77fBslEs3b--B7xbCaxtEoP0obqTD7VMBCtW/exec');
+  await fetch('https://script.google.com/macros/s/AKfycbw5DW0zkMVIzPEIn4pAYnAgklIhVAXR77fBslEs3b--B7xbCaxtEoP0obqTD7VMBCtW/exec');
   return {
     statusCode: 200,
     body: 'Request accepted.',
