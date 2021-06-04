b3b5231f20500f2953240bbe74ba185fba764432
Yu MORIYA <euro21st@gmail.com>
2021-06-04 22:42:52 +0900

    Add sleep

--- netlify/functions/gas.js
+++ netlify/functions/gas.js
@@ -1,7 +1,10 @@
 const fetch = require('node-fetch');
 
+const _sleep = (ms) => new Promise((resolve) => setTimeout(resolve, ms));
+
 exports.handler = async (event, context) => {
-  let promise = fetch('https://script.google.com/macros/s/AKfycbw5DW0zkMVIzPEIn4pAYnAgklIhVAXR77fBslEs3b--B7xbCaxtEoP0obqTD7VMBCtW/exec');
+  fetch('https://script.google.com/macros/s/AKfycbw5DW0zkMVIzPEIn4pAYnAgklIhVAXR77fBslEs3b--B7xbCaxtEoP0obqTD7VMBCtW/exec');
+  _sleep(2000);
   return {
     statusCode: 200,
     body: 'Request accepted.',
