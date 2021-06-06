391e54f02b4a793f7e682d1c3a5222480463b426
Yu MORIYA <euro21st@gmail.com>
2021-06-06 10:53:20 +0900

    Update webhook url

--- netlify/functions/gas.js
+++ netlify/functions/gas.js
@@ -3,7 +3,7 @@ const fetch = require('node-fetch');
 const _sleep = (ms) => new Promise((resolve) => setTimeout(resolve, ms));
 
 exports.handler = async (event, context) => {
-  fetch('https://script.google.com/macros/s/AKfycbw5DW0zkMVIzPEIn4pAYnAgklIhVAXR77fBslEs3b--B7xbCaxtEoP0obqTD7VMBCtW/exec');
+  fetch('https://script.google.com/macros/s/AKfycby1pjttwtClrDnqy7hRffTjPp_WxMnMnb1zbplyOJEpOykegifMv5GzQwx2RTrSYyLf/exec');
   await _sleep(2000);
   return {
     statusCode: 200,
